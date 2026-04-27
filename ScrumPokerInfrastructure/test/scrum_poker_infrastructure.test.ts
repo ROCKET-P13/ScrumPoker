363a1d02d0d83b417c6328ea9a874e47f82f5c68
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-04-27 06:21:18 -0400

    feat: ScrumPokerInfrastructure

---
+++ ScrumPokerInfrastructure/test/scrum_poker_infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ScrumPokerInfrastructure from '../lib/scrum_poker_infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/scrum_poker_infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ScrumPokerInfrastructure.ScrumPokerInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
